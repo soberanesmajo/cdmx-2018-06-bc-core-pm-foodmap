d21543ed8dc225c724b3c8097cbbfc4bfad0c8d8
soberanesmajo <soberanesmajo@gmail.com>
2018-08-06 18:03:45 -0500

    agrega splash y actualización de diseño

--- js/app.js
+++ js/app.js
@@ -1,10 +1,41 @@
-let map;
+var map;
 
-function initMap() {
+function initialize() {
+  var center = new google.maps.LatLng(19.4326077, -99.13320799999997);
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 19.4326077, lng: -99.13320799999997},
-    zoom: 13
+    center: center,
+    zoom: 12
   });
+
+
+var request = {
+  location : center,
+  radius: 8000,
+  types: ['bar']
+};
+
+var service = new google.maps.places.PlacesService(map);
+
+service.nearbySearch(request, callback);
+
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+const callback = (results, status) => {
+  if(status === google.maps.places.PlacesServiceStatus.OK){
+    for(i=0; i<results.length; i++){
+      createMarker(results[i]);
+      console.log(results[i]);
+    }
+  }
+}
+
+const createMarker = (place) => {
+  let placeLoc = place.geometry.location;
+  let market = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+ });
+}
+
+
+//google.maps.event.addDomListener(window, 'load', initialize);
---
+++ js/splash.js
@@ -0,0 +1,5 @@
+const viewSplash = () => {
+    location.href = 'views/view1.html';
+}
+
+setTimeout('viewSplash()', 2000);
